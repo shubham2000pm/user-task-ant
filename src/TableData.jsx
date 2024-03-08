3f4aec561eb8372ea8a95b95a87cc476b0c4923e
shubham <shunham.sourav@corp.knowledgelens.com>
2024-03-08 16:32:38 +0530

    ant-desing

--- src/TableData.jsx
+++ src/TableData.jsx
@@ -202,6 +202,7 @@ const TableData = () => {
             showTotal: (total, range) =>
               `${range[0]}-${range[1]} of ${total} items`,
           }}
+          scroll={{ x: true }}
         />
 
         <Modal
